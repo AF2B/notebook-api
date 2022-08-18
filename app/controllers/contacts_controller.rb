0e48036afc299d2917906d46a495fa5664c8a270
andre.borba <dev_andre.filipe@outlook.com>
2022-08-18 17:40:21 -0300

    adicionado kind.description para /:id contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,10 +10,9 @@ class ContactsController < ApplicationController
   end
 
   def show
-    render json: @contact
+    render json: @contact.as_json.merge(kind: @contact.kind.description)
   end
 
-  # POST /contacts
   def create
     @contact = Contact.new(contact_params)
 
@@ -24,7 +23,6 @@ class ContactsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contacts/1
   def update
     if @contact.update(contact_params)
       render json: @contact
@@ -33,19 +31,16 @@ class ContactsController < ApplicationController
     end
   end
 
-  # DELETE /contacts/1
   def destroy
     @contact.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_contact
     @contact = Contact.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def contact_params
     params.require(:contact).permit(:name, :email, :birthdate)
   end
