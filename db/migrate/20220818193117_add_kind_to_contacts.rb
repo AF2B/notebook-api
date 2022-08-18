2823176d12ae7f771715f923329d70b7b5445876
andre.borba <dev_andre.filipe@outlook.com>
2022-08-18 16:32:22 -0300

    consertado bug de referencia kind to contacts

--- db/migrate/20220818193117_add_kind_to_contacts.rb
+++ db/migrate/20220818193117_add_kind_to_contacts.rb
@@ -1,4 +1,4 @@
-class AddKindToContact < ActiveRecord::Migration[7.0]
+class AddKindToContacts < ActiveRecord::Migration[7.0]
   def change
     add_reference :contacts, :kind, null: false, foreign_key: true
   end
