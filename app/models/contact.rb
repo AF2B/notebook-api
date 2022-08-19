aab98bbc53ae3d163adfb8f7284f29742f05e187
andre.borba <dev_andre.filipe@outlook.com>
2022-08-19 00:32:34 -0300

    model refatorado

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,6 +5,15 @@ class Contact < ApplicationRecord
     I18n.localize(birthdate) unless birthdate.blank?
   end
 
+  def to_ptbr
+    {
+      name: name,
+      email: email,
+      birthdate: birthdate_formatted_ptbr,
+      kind: kind.description
+    }
+  end
+
   def as_json(_options = {})
     super(except: %i[created_at updated_at kind_id])
   end
