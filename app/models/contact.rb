006428e94e506cec9e0220fa5b36307f4da258c8
andre.borba <dev_andre.filipe@outlook.com>
2022-08-19 00:15:46 -0300

    metodo criado i18n data formatada

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :kind
 
+  def birthdate_formatted_ptbr
+    I18n.localize(birthdate) unless birthdate.blank?
+  end
+
   def as_json(_options = {})
     super(except: %i[created_at updated_at kind_id])
   end
