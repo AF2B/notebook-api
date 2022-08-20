bc359ca64606e720644f57fef5a070ff3d8f4a6f
andre.borba <dev_andre.filipe@outlook.com>
2022-08-20 02:57:53 -0300

    criado associação em contact de address

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
+  has_one :address
 
   accepts_nested_attributes_for :phones, allow_destroy: true
+  accepts_nested_attributes_for :address
 
   def as_json(_options = {})
     hash = super(except: %i[created_at updated_at kind_id id])
