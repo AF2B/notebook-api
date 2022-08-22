70246e33167e81a1af533cb0616b36a889a1d635
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 13:26:02 -0300

    limpeza do controller kinds

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,18 +1,16 @@
 class KindsController < ApplicationController
   before_action :set_kind, only: %i[show update destroy]
-  # GET /kinds
+
   def index
     @kinds = Kind.all
 
     render json: @kinds
   end
 
-  # GET /kinds/1
   def show
     render json: @kind
   end
 
-  # POST /kinds
   def create
     @kind = Kind.new(kind_params)
 
@@ -23,7 +21,6 @@ class KindsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /kinds/1
   def update
     if @kind.update(kind_params)
       render json: @kind
@@ -32,19 +29,16 @@ class KindsController < ApplicationController
     end
   end
 
-  # DELETE /kinds/1
   def destroy
     @kind.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_kind
     @kind = Kind.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def kind_params
     params.require(:kind).permit(:description)
   end
