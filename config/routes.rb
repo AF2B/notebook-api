1e10f70d441de225f2858f95437dd7eb300037a4
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 18:08:35 -0300

    destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resource :phones, only: %i[show]
     resource :phones, only: %i[show], path: 'relationships/phones'
 
-    resource :address, only: %i[show update create]
-    resource :address, only: %i[show update create], path: 'relationships/address'
+    resource :address, only: %i[show update create destroy]
+    resource :address, only: %i[show update create destroy], path: 'relationships/address'
   end
 end
