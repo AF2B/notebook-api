5df11c814c53a3f8232593e2fe78b3ec7d67acc5
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 15:42:15 -0300

    rotas configuradas no padrão jsonapi

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :contacts do
     resource :kind, only: %i[show]
     resource :kind, only: %i[show], path: 'relationships/kind'
+
+    resource :phones, only: %i[show]
+    resource :phones, only: %i[show], path: 'relationships/phones'
   end
 end
