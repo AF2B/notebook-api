ecc854ff4718f188f3a47d1a26f96859ce74380b
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 01:44:04 -0300

    adicionado before_destroy relações

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,24 +1,29 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  before_destroy :destroy_phones
+  before_destroy :destroy_addresses
+
+  belongs_to :kind, optional: true
   has_many :phones
   has_one :address
 
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
 
-  # def as_json(_options = {})
-  #   hash = super(except: %i[created_at updated_at kind_id id])
-  #   hash[:birthdate] = birthdate_formatted_ptbr
-  #   hash[:kind] = kind.description
-  #   hash[:phones] = phones.as_json(only: %i[number id])
-  #   hash[:address] = address.as_json(only: %i[street city])
-  #   hash
-  # end
   def birthdate_formatted_ptbr
     I18n.localize(birthdate) unless birthdate.blank?
   end
 
   def locale_to_ptbr
     I18n.locale = :pt_BR
+  end
+
+  private
+
+  def destroy_phones
+    phones.destroy_all
+  end
+
+  def destroy_addresses
+    address.destroy
   end
 end
