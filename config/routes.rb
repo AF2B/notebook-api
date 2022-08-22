eababbebcd41a03d730e7d9da20b027bca2b0d18
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 13:25:37 -0300

    refatorado para padrão apijson

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :contacts do
+    resource :kind, only: %i[show]
+  end
 end
