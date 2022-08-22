3af9f5457533e7865fc1aed38a4364d1e6f3786d
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 00:23:55 -0300

    correção nome associação kind para related

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,7 +2,7 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind do
-    link(:kind) { kind_url(object.kind) }
+    link(:related) { kind_url(object.kind) }
   end
   has_many :phones
   has_one :address
