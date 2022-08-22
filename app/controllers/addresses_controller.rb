02a0c103b9514a1e3933f4183c106260fc0e6df0
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 17:33:13 -0300

    criado a action update

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,14 @@
 class AddressesController < ApplicationController
   before_action :set_address, only: %i[show]
+  before_action :set_contact, only: %i[update]
+
+  def update
+    if @contact.address.update(address_params)
+      render json: @contact.address, status: :ok, message: 'Address updated'
+    else
+      render json: @contact.address.errors, status: :unprocessable_entity
+    end
+  end
 
   def show
     render json: @address
@@ -14,5 +23,13 @@ class AddressesController < ApplicationController
     end
 
     @address = Address.find(params[:id])
+  end
+
+  def set_contact
+    @contact = Contact.find(params[:contact_id])
+  end
+
+  def address_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
   end
 end
