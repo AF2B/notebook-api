a7ebdb95ab9510d2607efa4592281885e3f3247b
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 14:22:35 -0300

    refatoração apijson.org

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :kinds
   resources :contacts do
     resource :kind, only: %i[show]
+    resource :kind, only: %i[show], path: 'relationships/kind'
   end
 end
