e9f7010f51ced3d0cd6164b31d0fbbf3f7285a55
andre.borba <dev_andre.filipe@outlook.com>
2022-08-22 15:55:25 -0300

    rotas para address

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
 
     resource :phones, only: %i[show]
     resource :phones, only: %i[show], path: 'relationships/phones'
+
+    resource :address, only: %i[show]
+    resource :address, only: %i[show], path: 'relationships/address'
   end
 end
