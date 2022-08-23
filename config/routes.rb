af0a301572538a859c9f307e28e16cec1fbcea42
andre.borba <dev_andre.filipe@outlook.com>
2022-08-23 02:06:10 -0300

    rotas para POST phone

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     resource :kind, only: %i[show], path: 'relationships/kind'
 
     resource :phones, only: %i[show]
+    resource :phone, only: %i[create update destroy]
     resource :phones, only: %i[show], path: 'relationships/phones'
+    resource :phone, only: %i[create update destroy], path: 'relationships/phones'
 
     resource :address, only: %i[show update create destroy]
     resource :address, only: %i[show update create destroy], path: 'relationships/address'
