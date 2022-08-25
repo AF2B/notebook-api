f793695de59a04dff3fa6144bf0b3168d87c65ad
andre.borba <dev_andre.filipe@outlook.com>
2022-08-24 22:34:37 -0300

    fix: contact_phonesa_url to v1

--- app/serializers/phone_serializer.rb
+++ app/serializers/phone_serializer.rb
@@ -2,6 +2,6 @@ class PhoneSerializer < ActiveModel::Serializer
   attributes :id, :number
 
   belongs_to :contact do
-    link(:related) { contact_phones_url(object.contact) }
+    link(:related) { v1_contact_phones_url(object.contact) }
   end
 end
