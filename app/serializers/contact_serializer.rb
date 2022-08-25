71dc516e1b592affccd6ff3c8b621616bd4f35af
andre.borba <dev_andre.filipe@outlook.com>
2022-08-24 22:34:20 -0300

    feat: serializer data kind, phones and address v2

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,20 +2,30 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind do
-    link(:related) { contact_kind_url(object.id) }
+    link(:related) { v1_contact_kind_url(object.id) }
   end
   has_many :phones do
-    link(:related) { contact_phones_url(object.id) }
+    link(:related) { v1_contact_phones_url(object.id) }
   end
   has_one :address do
-    link(:related) { contact_address_url(object.id) }
+    link(:related) { v1_contact_address_url(object.id) }
   end
 
-  link(:self) { contact_url(object) }
+  belongs_to :kind, serializer: KindSerializer, root: :kind do
+    link(:related) { v2_contact_kind_url(object.id) }
+  end
+  has_many :phones, serializer: PhoneSerializer, root: :phones do
+    link(:related) { v2_contact_phones_url(object.id) }
+  end
+  has_one :address, serializer: AddressSerializer, root: :address do
+    link(:related) { v2_contact_address_url(object.id) }
+  end
+
+  link(:self) { v1_contact_url(object) }
 
   def attributes(*args)
     hash = super(*args)
-    hash[:kind] = object.kind.description
+    # hash[:kind] = object.kind.description
     hash[:phones] = object.phones.map(&:number)
     hash[:address] = object.address.street if object.address.present?
     hash[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
