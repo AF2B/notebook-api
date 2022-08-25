89c951d54c84b93819ef4bde2aef7f163f3657be
andre.borba <dev_andre.filipe@outlook.com>
2022-08-24 22:35:12 -0300

    feat: added versionist api_version to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,37 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts do
-    resource :kind, only: %i[show]
-    resource :kind, only: %i[show], path: 'relationships/kind'
 
-    resource :phones, only: %i[show]
-    resource :phone, only: %i[create update destroy]
-    resource :phones, only: %i[show], path: 'relationships/phones'
-    resource :phone, only: %i[create update destroy], path: 'relationships/phones'
+  api_version(module: 'V1', path: { value: 'v1' }) do
+    resources :contacts do
+      resource :kind, only: %i[show]
+      resource :kind, only: %i[show], path: 'relationships/kind'
 
-    resource :address, only: %i[show update create destroy]
-    resource :address, only: %i[show update create destroy], path: 'relationships/address'
+      resource :phones, only: %i[show]
+      resource :phone, only: %i[create update destroy]
+      resource :phones, only: %i[show], path: 'relationships/phones'
+      resource :phone, only: %i[create update destroy], path: 'relationships/phones'
 
-    get '/api' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.json')
+      resource :address, only: %i[show update create destroy]
+      resource :address, only: %i[show update create destroy], path: 'relationships/address'
+
+      # get '/api' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.json')
+    end
+  end
+
+  api_version(module: 'V2', path: { value: 'v2' }) do
+    resources :contacts do
+      resource :kind, only: %i[show]
+      resource :kind, only: %i[show], path: 'relationships/kind'
+
+      resource :phones, only: %i[show]
+      resource :phone, only: %i[create update destroy]
+      resource :phones, only: %i[show], path: 'relationships/phones'
+      resource :phone, only: %i[create update destroy], path: 'relationships/phones'
+
+      resource :address, only: %i[show update create destroy]
+      resource :address, only: %i[show update create destroy], path: 'relationships/address'
+
+      # get '/api' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.json')
+    end
   end
 end
