f5157577c014e4743813840bba5f413fb4cc38fd
andre.borba <dev_andre.filipe@outlook.com>
2022-08-24 22:30:03 -0300

    feat: added v1 kinds_controller

---
+++ app/controllers/v1/kinds_controller.rb
@@ -0,0 +1,60 @@
+class V1::KindsController < ApplicationController
+  before_action :set_kind, only: %i[show update destroy]
+
+  def index
+    @kinds = Kind.all
+
+    render json: @kinds
+  end
+
+  def show
+    render json: @kind
+  end
+
+  def create
+    @kind = Kind.new(kind_params)
+
+    if @kind.save
+      render json: @kind, status: :created, location: @kind
+    else
+      render json: @kind.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @kind.update(kind_params)
+      render json: @kind
+    else
+      render json: @kind.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kind.destroy
+  end
+
+  private
+
+  def set_kind
+    if params[:contact_id]
+      @kind = Contact.find(params[:contact_id]).kind_id
+      return @kind
+    end
+
+    @kind = Kind.find(params[:id])
+  end
+
+  # def set_kind
+  #   kind_id = if params[:contact_id]
+  #               Contact.find(params[:contact_id]).kind_id
+  #             else
+  #               Kind.find(params[:id])
+  #             end
+
+  #   @kind = Kind.find(kind_id)
+  # end
+
+  def kind_params
+    params.require(:kind).permit(:description)
+  end
+end
