74825fd4c3c1b3b554d7f317f8f46e849385c09c
andre.borba <dev_andre.filipe@outlook.com>
2022-08-24 22:28:41 -0300

    feat: auth requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::API
-  before_action :ensure_json_request # before_filter to old version of Rails
+  before_action :ensure_json_request
 
   def ensure_json_request
-    return if request.headers['Accept'].include?('application/json')
+    if request.headers['Accept'] =~ /vnd\.api\+json/
+      unless request.get?
+        return if request.headers['Content-Type'] =~ /vnd\.api\+json/
 
-    render nothing: true, status: 406
+        render nothing: true, status: 415
+      end
+    else
+      render nothing: true, status: 406
+    end
   end
+
+  # def ensure_json_request
+  #   if request.headers['Accept'].include?('application/json')
+  #     unless request.headers['Content-Type'].include?('application/json')
+  #       render json: { error: 'Invalid Content-Type header' }, status: :unsupported_media_type
+  #     end
+  #   else
+  #     render json: { error: 'Invalid Accept header' }, status: :not_acceptable
+  #   end
+  # end
 end
