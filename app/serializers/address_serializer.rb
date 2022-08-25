3dd055ed01ea989f43dc5a228d6686f1d8c0329a
andre.borba <dev_andre.filipe@outlook.com>
2022-08-24 22:33:49 -0300

    fix: changed contact_address_url to v1

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -2,6 +2,10 @@ class AddressSerializer < ActiveModel::Serializer
   attributes :id, :street, :city
 
   belongs_to :contact do
-    link(:related) { contact_address_url(object.contact) }
+    link(:related) { v1_contact_address_url(object.contact) }
   end
+
+  # belongs_to :kind do
+  #   link(:related) { v2_contact_address_url(object.contact) }
+  # end
 end
